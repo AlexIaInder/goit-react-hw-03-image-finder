87adcf81f87c7e8778032d432e153e1482f01ba9
Alexander Andriyash <olexandr.andriyash@gmail.com>
2022-09-21 19:59:03 +0300

    update

---
+++ src/components/ImageGallery/index.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+import css from './ImageGallery.module.css';
+
+class ImageGallery extends Component {
+  render() {
+    return <ul class={css.ImageGallery}></ul>;
+  }
+}
+export default ImageGallery;
